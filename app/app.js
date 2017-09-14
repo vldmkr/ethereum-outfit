61a5bf9a0e0589f1249ad5d685b2e4de57f67f61
vldmkr <vladmakaryan@gmail.com>
2017-09-15 02:42:41 +0300

    using local storage to save items

--- app/app.js
+++ app/app.js
@@ -1,7 +1,35 @@
+(function () {
+  var items = []
+
+  this.deserializeItems = function () {
+    items = JSON.parse(localStorage.getItem('items')) || []
+    items.forEach(function(item) {
+      drawItem(item.title, item.url, item.subtitle, true)
+    });
+  }
+
+  this.serializeItems = function () {
+    localStorage.setItem('items', JSON.stringify(items))
+  }
+
+  this.drawItem = function (title, url, subtitle, ignore) {
+    var item = { 
+      title: title, 
+      url: url, 
+      subtitle: subtitle
+    }
+    document.getElementById('tx-list').innerHTML += template.txItem(item)
+    if( ! ignore) {
+      items.push(item)
+      serializeItems()
+    }
+  }
+})()
+
 window.addEventListener('load', function () {
+  deserializeItems()
   if (typeof web3 !== 'undefined') {
     window.web3 = new Web3(web3.currentProvider)
-
     web3.net.getListening( function(error, result) { 
       if( ! error && result) {
         document.getElementById('input-claim').placeholder = web3.eth.defaultAccount
@@ -14,12 +42,6 @@ window.addEventListener('load', function () {
   }
 });
 
-document.getElementById('tx-list').innerHTML += template.txItem({ 
-  title: 'Token Address', 
-  url: 'https://ropsten.etherscan.io/token/' + token.contractAddress, 
-  subtitle: token.contractAddress
-})
-
 document.body.innerHTML += template.forkMe({ 
   url: 'https://github.com/vldmkr/erc20-token-boilerplate'
 })
@@ -30,17 +52,9 @@ document.getElementById('btn-send').onclick = function () {
     document.getElementById("input-amount").value,
     function (error, result) {
       if ( ! error) {
-        document.getElementById('tx-list').innerHTML += template.txItem({ 
-          title: 'Send', 
-          url: 'https://ropsten.etherscan.io/tx/' + result, 
-          subtitle: result
-        })
+        drawItem('Send', 'https://ropsten.etherscan.io/tx/' + result, result)
       } else {
-        document.getElementById('tx-list').innerHTML += template.txItem({ 
-          title: 'Send', 
-          url: '#send', 
-          subtitle: error.message.split('\n')[0]
-        })
+        drawItem('Send', '#send', error.message.split('\n')[0])
       }
     })
 }
@@ -49,27 +63,21 @@ document.getElementById('btn-claim').onclick = function () {
   web3.eth.contract(token.contractABI).at(token.contractAddress).claim(
     function (error, result) {
       if ( ! error) {
-        document.getElementById('tx-list').innerHTML += template.txItem({ 
-          title: 'Claim', 
-          url: 'https://ropsten.etherscan.io/tx/' + result, 
-          subtitle: result
-        })
+        drawItem('Claim', 'https://ropsten.etherscan.io/tx/' + result, result)
       } else {
-        document.getElementById('tx-list').innerHTML += template.txItem({ 
-          title: 'Claim', 
-          url: '#claim', 
-          subtitle: error.message.split('\n')[0]
-        })
+        drawItem('Claim', '#claim', error.message.split('\n')[0])
       }
     })
 }
 
+drawItem('Token Address', 'https://ropsten.etherscan.io/token/' + token.contractAddress, token.contractAddress, true)
+
 route('send', function (isActive) {
   document.getElementById('send-form').style.display = isActive ? 'block' : 'none'
   document.getElementById('send-ref').className = isActive ? 'active' : ''
-});
+})
 
 route('claim', function (isActive) {
   document.getElementById('claim-form').style.display = isActive ? 'block' : 'none'
   document.getElementById('claim-ref').className = isActive ? 'active' : ''
-});
+})
