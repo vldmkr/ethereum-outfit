28c3d3d89f250b02a44cfe557cab0b1a33824be2
vldmkr <vladmakaryan@gmail.com>
2017-10-26 15:56:09 +0300

    ContractIoutfit implemented, refactoring

--- deploy-service.js
+++ deploy-service.js
@@ -6,11 +6,13 @@ const account = require('./account')
 
 const Web3Provider = require('./lib/Web3Provider')
 const ContractsCompiler = require('./lib/ContractsCompiler')
+const ContractsOutfit = require('./lib/ContractsOutfit')
 const WorkingQueue = require('./lib/WorkingQueue')
 
 const app = express()
 const web3 = Web3Provider().web3
 const compiler = ContractsCompiler(path.join(__dirname, 'contracts'))
+const outfit = ContractsOutfit(web3)
 const requestQueue = WorkingQueue()
 
 app.use(bodyParser.json());
@@ -58,7 +60,8 @@ function doWork (params) {
     doRequest(options, false)
   }
 
-  compiler.deploy(web3, account.address, "GenCrowdsale.sol:GenCrowdsale", params)
+  compiler.compile(params)
+  .then(output => outfit.deploy(output.contracts["GenCrowdsale.sol:GenCrowdsale"], account.address))
   .then(emiter => {
     emiter.on('transactionHash', transactionHash => {
       options.json["tx"] = "https://ropsten.etherscan.io/tx/" + transactionHash
